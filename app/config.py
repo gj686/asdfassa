e6febf5a129b8cb105d373fcb28c502a7b525623
gj686 <jay.gandhi5709@marwadieducation.edu.in>
2018-09-12 19:24:40 +0530

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018 Amir Hadifar. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+# todo: replace all following key with yours
+
+# Generate API_KEY and API_SECRET from ==> https://apps.twitter.com
+API_KEY = "eB1aTOyqYKtvVQc2iVlyg1CL3"
+API_SECRET = "hqEkZaJk0yittXWYkWa2Hx72YophngEL6z7nPWBFQGfEBuwxlv"
+
+# In Keys and access tokens click on generate access token button
+ACCESS_TOKEN = "941030573128040448-oR3K81rCUTO54ZZOg5Z5WA3SoTrYhEq"
+ACCESS_TOKEN_SECRET = "PLG99Rq2dGgfegADHzFQrJo40nCqD9ah5ygEHWgdKhiWI"
+
+
+# channel name and Bot API key ==> https://core.telegram.org/bots/api
+TELEGRAM_CHANNEL_NAME = "@cryptoslack"
+TELEGRAM_BOT_API_KEY = "695404392:AAHt5Th2xSiD-lGkiN4tOA9IcbN0xoicWqg"
